9eefdd3dd2d76fe76c314c7a0b6acc47ade59c38
Sanaz <szeraati69@gmail.com>
2022-12-16 23:38:23 +0330

    add styles

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,6 +3,10 @@ import { useSelector, useDispatch } from 'react-redux';
 import { loadTodo, deleteTodo } from '../redux/todo/todoAction';
 import { TODO_KEY } from '../redux/todo/todoReducer';
 import { v4 } from 'uuid';
+import { BsPlusLg } from 'react-icons/bs';
+import { BiMinus } from 'react-icons/bi';
+
+import styles from './Todo.module.css';
 
 const Todo = () => {
   //initialize dispatch
@@ -46,34 +50,44 @@ const Todo = () => {
 
   return (
     <>
-      <div>
-        <p>Todo App </p>
-      </div>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <input
-              name="myText"
-              type="text"
-              value={text.myText}
-              onChange={handleChange}
-              placeholder="Enter todos"
-            />
-            <button type="submit">add</button>
-          </div>
-        </form>
-      </div>
-      <div>
-        {items.map((todo) => {
-          return (
-            <div key={todo.id}>
-              <ul>
-                <li>{todo.todoItem.myText}</li>
-              </ul>
-              <button onClick={() => handleDelete(todo.id)}>delete</button>
+      <div className={styles.todoContainer}>
+        <div className={styles.topic}>
+          <p>Todo App </p>
+        </div>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <input
+                className={styles.inputContainer}
+                name="myText"
+                type="text"
+                value={text.myText}
+                onChange={handleChange}
+                placeholder="Enter todos"
+              />
+              <button type="submit">
+                <BsPlusLg />
+              </button>
             </div>
-          );
-        })}
+          </form>
+        </div>
+        <div>
+          {items.map((todo) => {
+            return (
+              <div key={todo.id} className={styles.listContainer}>
+                <ul>
+                  <li>{todo.todoItem.myText}</li>
+                </ul>
+                <button
+                  onClick={() => handleDelete(todo.id)}
+                  className={styles.deleteButton}
+                >
+                  <BiMinus />
+                </button>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
