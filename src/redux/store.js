38c040ee213ed94fd149eaade0325e8edb26a88d
Sanaz <szeraati69@gmail.com>
2023-01-08 18:48:21 +0330

    add localstorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,16 @@ import { createStore } from 'redux';
 
 import { rootReducer } from './rootReducer';
 
-const store = createStore(rootReducer);
+const persistentState = localStorage.getItem('state')
+  ? JSON.parse(localStorage.getItem('state'))
+  : {};
+
+const store = createStore(rootReducer, persistentState);
+
+store.subscribe(() => {
+  const state = store.getState();
+  const serializedState = JSON.stringify(state);
+  localStorage.setItem('state', serializedState);
+});
 
 export { store };
